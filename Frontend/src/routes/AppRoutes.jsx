d0c547396149fd870de34a339f5dce2a365779e2
Deepak Raikwar <raikwardeepak310@gmail.com>
2026-07-15 18:00:28 +0530

    Update Analytics

--- Frontend/src/routes/AppRoutes.jsx
+++ Frontend/src/routes/AppRoutes.jsx
@@ -8,7 +8,7 @@ import NotFoundPage from "../pages/NotFound/NotFoundPage";
 import MaintenancePage from "../pages/Maintenance/MaintenancePage";
 import ServerDownPage from "../pages/ServerDown/ServerDownPage";
 import ProjectsPage from "../pages/Project/ProjectsPage";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 
 
 export default function AppRoutes() {
